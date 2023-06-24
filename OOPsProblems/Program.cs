0c9a7ca8466dacadf41928da5e02436cc6b0fb46
Pankaj-Thorat <thoratpankaj282@gmail.com>
2023-06-24 21:21:17 +0530

    [Pankaj] Added example of OOPs concept

---
+++ OOPsProblems/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPsProblems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an object of the Rectangle class
+            Rectangle myRectangle = new Rectangle(4.5, 3.2);
+
+            // Access and modify object properties
+            Console.WriteLine("Length: " + myRectangle.Length);
+            Console.WriteLine("Width: " + myRectangle.Width);
+            Console.WriteLine("Area: " + myRectangle.CalculateArea());
+
+            // Modify object properties
+            myRectangle.Length = 5.1;
+            myRectangle.Width = 2.8;
+
+            // Access updated object properties
+            Console.WriteLine("Length: " + myRectangle.Length);
+            Console.WriteLine("Width: " + myRectangle.Width);
+            Console.WriteLine("Area: " + myRectangle.CalculateArea());
+        }
+    }
+}
---
+++ OOPsProblems/Rectangle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPsProblems
+{
+    public class Rectangle
+    {
+        // Fields
+        public double Length { get; set; }
+        public double Width { get; set; }
+
+        // Constructor
+        public Rectangle(double length, double width)
+        {
+            Length = length;
+            Width = width;
+        }
+
+        // Method to calculate the area
+        public double CalculateArea()
+        {
+            return Length * Width;
+        }
+    }
+}
